47e7e77c8074b91faf6efc0b4ec93f948f314245
TimCatana <timothycatana@gmail.com>
2022-11-10 21:56:17 -0500

    almost done student hook

--- frontend/src/screens/new-entry-screens/new-student-screen/useNewStudentScreen.js
+++ frontend/src/screens/new-entry-screens/new-student-screen/useNewStudentScreen.js
@@ -14,46 +14,76 @@ const useNewCourseScreen = () => {
   const [isStudentFirstNameError, _setIsStudentFirstNameError] = useState(true);
 
   const [studentMiddleName, _setStudentMiddleName] = useState("");
-  const [isStudentMiddleNameError, _setIsStudentMiddleNameError] = useState(true);
-  
+  const [isStudentMiddleNameError, _setIsStudentMiddleNameError] =
+    useState(true);
+
   const [studentLastName, _setStudentLastName] = useState("");
   const [isStudentLastNameError, _setIsStudentLastNameError] = useState(true);
 
   const [studentDateOfBirth, _setStudentDateOfBirth] = useState("");
-  const [isStudentDateOfBirthError, _setIsStudentDateOfBirthError] = useState(true);
+  const [isStudentDateOfBirthError, _setIsStudentDateOfBirthError] =
+    useState(true);
 
   const [studentGender, _setStudentGender] = useState(""); // should be 0 = M, 1 =F, 2 = Not Declared
   const [isStudentGenderError, _setIsStudentGenderError] = useState(true);
 
   const [studentCellPhoneNumber, _setStudentCellPhoneNumber] = useState("");
-  const [isStudentCellPhoneNumberError, _setIsStudentCellPhoneNumberError] = useState(true);
-  
+  const [isStudentCellPhoneNumberError, _setIsStudentCellPhoneNumberError] =
+    useState(true);
+
   const [studentHomePhoneNumber, _setStudentHomePhoneNumber] = useState("");
-  const [isStudentHomePhoneNumberError, _setIsStudentHomePhoneNumberError] = useState(true);
-  
+  const [isStudentHomePhoneNumberError, _setIsStudentHomePhoneNumberError] =
+    useState(true);
+
   const [studentAddress, _setStudentAddress] = useState("");
   const [isStudentAddressError, _setIsStudentAddressError] = useState(true);
-  
+
   const [studentAddressCity, _setStudentAddressCity] = useState("");
-  const [isStudentAddressCityError, _setIsStudentAddressCityError] = useState(true);
-  
+  const [isStudentAddressCityError, _setIsStudentAddressCityError] =
+    useState(true);
+
   const [studentAddressPostalCode, _setStudentAddressPostalCode] = useState("");
-  const [isStudentAddressPostalCodeError, _setIsStudentAddressPostalCodeError] = useState(true);
-  
-  const [studentDriversLicenseNumber, _setStudentDriversLicenseNumber] = useState("");
-  const [isStudentDriversLicenseNumberError, _setIsStudentDriversLicenseNumberError] = useState(true);
- 
-  const [studentDriversLicenseNumberIssuedDate, _setStudentDriversLicenseNumberIssuedDate] = useState("");
-  const [isStudentDriversLicenseNumberErrorIssuedDate, _setIsStudentDriversLicenseNumberErrorIssuedDate] = useState(true);
- 
-  const [studentDriversLicenseNumberExpDate, _setStudentDriversLicenseNumberExpDate] = useState("");
-  const [isStudentDriversLicenseNumberExpDateError, _setIsStudentDriversLicenseNumberExpDateError] = useState(true);
- 
-  const [studentRegisteredCourseId, _setStudentRegisteredCourseId] = useState("");
-  const [isStudentRegisteredCourseIdError, _setIsStudentRegisteredCourseIdError] = useState(true);
- 
-  const [studentRegisteredProductId, _setStudentRegisteredProductId] = useState("");
-  const [isStudentRegisteredProductIdError, _setIsStudentRegisteredProductIdError] = useState(true);
+  const [isStudentAddressPostalCodeError, _setIsStudentAddressPostalCodeError] =
+    useState(true);
+
+  const [studentDriversLicenseNumber, _setStudentDriversLicenseNumber] =
+    useState("");
+  const [
+    isStudentDriversLicenseNumberError,
+    _setIsStudentDriversLicenseNumberError,
+  ] = useState(true);
+
+  const [
+    studentDriversLicenseNumberIssuedDate,
+    _setStudentDriversLicenseNumberIssuedDate,
+  ] = useState("");
+  const [
+    isStudentDriversLicenseNumberIssuedDateError,
+    _setIsStudentDriversLicenseNumberIssuedDateError,
+  ] = useState(true);
+
+  const [
+    studentDriversLicenseNumberExpDate,
+    _setStudentDriversLicenseNumberExpDate,
+  ] = useState("");
+  const [
+    isStudentDriversLicenseNumberExpDateError,
+    _setIsStudentDriversLicenseNumberExpDateError,
+  ] = useState(true);
+
+  const [studentRegisteredCourseId, _setStudentRegisteredCourseId] =
+    useState("");
+  const [
+    isStudentRegisteredCourseIdError,
+    _setIsStudentRegisteredCourseIdError,
+  ] = useState(true);
+
+  const [studentRegisteredProductId, _setStudentRegisteredProductId] =
+    useState("");
+  const [
+    isStudentRegisteredProductIdError,
+    _setIsStudentRegisteredProductIdError,
+  ] = useState(true);
 
   /***********************/
   /***** USE EFFECTS *****/
@@ -64,62 +94,155 @@ const useNewCourseScreen = () => {
    * @dependent courseId
    */
   useEffect(() => {
-    courseId.length > 0
-      ? _setIsCourseIdError(false)
-      : _setIsCourseIdError(true);
-    // TODO - make sure its a valid number? HTML should take care of that. though I will validate again in the backend.
-  }, [courseId]);
+    studentFirstName.length > 0
+      ? _setIsStudentFirstNameError(false)
+      : _setIsStudentFirstNameError(true);
+  }, [studentFirstName]);
 
   /**
-   * Validates newly selected start date
-   * @dependent courseStartDate
+   * Validates newly inputted courseId
+   * @dependent courseId
    */
   useEffect(() => {
-    courseStartDate != null
-      ? _setIsCourseStartDateError(false)
-      : _setIsCourseStartDateError(true);
-    _setIsCourseStartDateError(!isDateValid(courseStartDate));
-  }, [courseStartDate]);
+    studentMiddleName.length > 0
+      ? _setIsStudentMiddleNameError(false)
+      : _setIsStudentMiddleNameError(true);
+  }, [studentMiddleName]);
+
+  /**
+   * Validates newly inputted courseId
+   * @dependent courseId
+   */
+  useEffect(() => {
+    studentLastName.length > 0
+      ? _setIsStudentLastNameError(false)
+      : _setIsStudentLastNameError(true);
+  }, [studentLastName]);
 
   /**
    * Validates newly selected end date date
    * @dependent courseEndDate
    */
   useEffect(() => {
-    courseEndDate != null ? _setIsCourseEndDateError(false) : _setIsCourseEndDateError(true);
-    _setIsCourseEndDateError(!isDateValid(courseEndDate));
-  }, [courseEndDate]);
+    studentDateOfBirth != null
+      ? _setIsStudentDateOfBirthError(false)
+      : _setIsStudentDateOfBirthError(true);
+    _setIsStudentDateOfBirthError(!isDateValid(studentDateOfBirth));
+  }, [studentDateOfBirth]);
 
   /**
-   * Validates newly selected is class digital or in person option
-   * @dependent isDigital
-   */
-  useEffect(() => {
-    _isCourseDigital === true || _isCourseDigital === false
-      ? _setIsCourseDigitalError(false)
-      : _setIsCourseDigitalError(true);
-  }, [_isCourseDigital]);
-
-  /**
-   * Validates newly inputted courseCapacity
+   * Validates newly inputted courseId
    * @dependent courseId
+   * 3 options M F Not-Declaring
    */
   useEffect(() => {
-    courseCapacity.length > 0
-      ? _setIsCourseCapacityError(false)
-      : _setIsCourseCapacityError(true);
-    // TODO - make sure its a valid number? HTML should take care of that. though I will validate again in the backend.
-  }, [courseCapacity]);
+    studentGender.length > 0
+      ? _setIsStudentGenderError(false)
+      : _setIsStudentGenderError(true);
+  }, [studentGender]);
 
   /**
-   * Validates newly selected in class instructor
-   * @dependent inClassInstructor
+   * Validates newly inputted courseId
+   * @dependent courseId
+   * 3 options M F Not-Declaring
    */
   useEffect(() => {
-    _courseInClassInstructor.length > 0
-      ? _setIsCourseInClassInstructorError(false)
-      : _setIsCourseInClassInstructorError(true);
-  }, [_courseInClassInstructor]);
+    studentCellPhoneNumber.length > 0
+      ? _setIsStudentCellPhoneNumberError(false)
+      : _setIsStudentCellPhoneNumberError(true);
+  }, [studentCellPhoneNumber]);
+
+  /**
+   * Validates newly inputted courseId
+   * @dependent courseId
+   * 3 options M F Not-Declaring
+   */
+  useEffect(() => {
+    studentHomePhoneNumber.length > 0
+      ? _setIsStudentHomePhoneNumberError(false)
+      : _setIsStudentHomePhoneNumberError(true);
+  }, [studentHomePhoneNumber]);
+
+  /**
+   * Validates newly inputted courseId
+   * @dependent courseId
+   * 3 options M F Not-Declaring
+   */
+  useEffect(() => {
+    studentAddress.length > 0
+      ? _setIsStudentAddressError(false)
+      : _setIsStudentAddressError(true);
+  }, [studentAddress]);
+
+  /**
+   * Validates newly inputted courseId
+   * @dependent courseId
+   * 3 options M F Not-Declaring
+   */
+  useEffect(() => {
+    studentAddressCity.length > 0
+      ? _setIsStudentAddressCityError(false)
+      : _setIsStudentAddressCityError(true);
+  }, [studentAddressCity]);
+
+  /**
+   * Validates newly inputted courseId
+   * @dependent courseId
+   * 3 options M F Not-Declaring
+   */
+  useEffect(() => {
+    studentDriversLicenseNumber.length > 0
+      ? _setIsStudentDriversLicenseNumberError(false)
+      : _setIsStudentDriversLicenseNumberError(true);
+  }, [studentDriversLicenseNumber]);
+
+  /**
+   * Validates newly selected end date date
+   * @dependent courseEndDate
+   */
+  useEffect(() => {
+    studentDriversLicenseNumberIssuedDate != null
+      ? _setIsStudentDriversLicenseNumberIssuedDateError(false)
+      : _setIsStudentDriversLicenseNumberIssuedDateError(true);
+    _setIsStudentDriversLicenseNumberIssuedDateError(
+      !isDateValid(studentDriversLicenseNumberIssuedDate)
+    );
+  }, [studentDriversLicenseNumberIssuedDate]);
+
+  /**
+   * Validates newly selected end date date
+   * @dependent courseEndDate
+   */
+  useEffect(() => {
+    studentDriversLicenseNumberExpDate != null
+      ? _setIsStudentDriversLicenseNumberExpDateError(false)
+      : _setIsStudentDriversLicenseNumberExpDateError(true);
+    _setIsStudentDriversLicenseNumberExpDateError(
+      !isDateValid(studentDriversLicenseNumberExpDate)
+    );
+  }, [studentDriversLicenseNumberExpDate]);
+
+  /**
+   * Validates newly inputted courseId
+   * @dependent courseId
+   * 3 options M F Not-Declaring
+   */
+  useEffect(() => {
+    studentRegisteredCourseId.length > 0
+      ? _setIsStudentRegisteredCourseIdError(false)
+      : _setIsStudentRegisteredCourseIdError(true);
+  }, [studentRegisteredCourseId]);
+
+  /**
+   * Validates newly inputted courseId
+   * @dependent courseId
+   * 3 options M F Not-Declaring
+   */
+  useEffect(() => {
+    studentRegisteredProductId.length > 0
+      ? _setIsStudentRegisteredProductIdError(false)
+      : _setIsStudentRegisteredProductIdError(true);
+  }, [studentRegisteredProductId]);
 
   /******************************/
   /***** USE EFFECT HELPERS *****/
@@ -129,58 +252,58 @@ const useNewCourseScreen = () => {
   /***** TEXT INPUTS *****/
   /***********************/
 
-  /**
-   * Updates the courseId variable to contain the newly inputted value
-   * @param value (string) The value inputted into the textInput
-   */
-  const handleCourseIdChange = (e) => {
-    e.preventDefault();
-    _setCourseId(e.target.value);
-  };
+  // /**
+  //  * Updates the courseId variable to contain the newly inputted value
+  //  * @param value (string) The value inputted into the textInput
+  //  */
+  // const handleCourseIdChange = (e) => {
+  //   e.preventDefault();
+  //   _setCourseId(e.target.value);
+  // };
 
-  /**
-   * Updates the start date variable to contain the newly selected value
-   * @param value (string) The value inputted into the textInput
-   */
-  const handleCourseStartDateChange = (e) => {
-    e.preventDefault();
-    _setCourseStartDate(e.target.value);
-  };
+  // /**
+  //  * Updates the start date variable to contain the newly selected value
+  //  * @param value (string) The value inputted into the textInput
+  //  */
+  // const handleCourseStartDateChange = (e) => {
+  //   e.preventDefault();
+  //   _setCourseStartDate(e.target.value);
+  // };
 
-  /**
-   * Updates the end date variable to contain the newly selected value
-   * @param value (string) The value inputted into the textInput
-   */
-  const handleCourseEndDateChange = (e) => {
-    e.preventDefault();
-    _setCourseEndDate(e.target.value);
-  };
+  // /**
+  //  * Updates the end date variable to contain the newly selected value
+  //  * @param value (string) The value inputted into the textInput
+  //  */
+  // const handleCourseEndDateChange = (e) => {
+  //   e.preventDefault();
+  //   _setCourseEndDate(e.target.value);
+  // };
 
-  /**
-   * Updates the isDigital variable to contain the newly selected value
-   * @param value (string) The value inputted into the textInput
-   */
-  const handleIsCourseDigitalChange = (e) => {
-    e.preventDefault();
-    _setIsCourseDigital(e.target.value);
-  };
+  // /**
+  //  * Updates the isDigital variable to contain the newly selected value
+  //  * @param value (string) The value inputted into the textInput
+  //  */
+  // const handleIsCourseDigitalChange = (e) => {
+  //   e.preventDefault();
+  //   _setIsCourseDigital(e.target.value);
+  // };
 
-  /**
-   * Updates the courseCapacity to contain the newly inputted value
-   * @param value (string) The value inputted into the textInput
-   */
-  const handleCourseCapacityChange = (e) => {
-    e.preventDefault();
-    _setCourseCapacity(e.target.value);
-  };
+  // /**
+  //  * Updates the courseCapacity to contain the newly inputted value
+  //  * @param value (string) The value inputted into the textInput
+  //  */
+  // const handleCourseCapacityChange = (e) => {
+  //   e.preventDefault();
+  //   _setCourseCapacity(e.target.value);
+  // };
 
-  /**
-   * Updates the inClassInstructor to contain the newly selected value
-   * @param value (string) The value inputted into the textInput
-   */
-  const handleInClassInstructorChange = (value) => {
-    _setCourseInClassInstructor(value);
-  };
+  // /**
+  //  * Updates the inClassInstructor to contain the newly selected value
+  //  * @param value (string) The value inputted into the textInput
+  //  */
+  // const handleInClassInstructorChange = (value) => {
+  //   _setCourseInClassInstructor(value);
+  // };
 
   /*************************/
   /***** BUTTON CLICKS *****/
@@ -234,23 +357,6 @@ const useNewCourseScreen = () => {
 
   return {
     isLoading,
-    courseId,
-    handleCourseIdChange,
-    isCourseIdError,
-    courseStartDate,
-    handleCourseStartDateChange,
-    isCourseStartDateError,
-    courseEndDate,
-    handleCourseEndDateChange,
-    isCourseEndDateError,
-    handleIsCourseDigitalChange,
-    isCourseDigitalError,
-    courseCapacity,
-    handleCourseCapacityChange,
-    isCourseCapacityError,
-    handleInClassInstructorChange,
-    isCourseInClassInstructorError,
-    handleAddNewCourseEntry,
   };
 };
 
